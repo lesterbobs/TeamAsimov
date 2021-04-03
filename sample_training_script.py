c3a99158811edaea25cdc32ca5293379df2102bc
lesterbobs <80299385+lesterbobs@users.noreply.github.com>
2021-04-03 05:40:33 -0400

    Update sample_training_script.py

--- sample_training_script.py
+++ sample_training_script.py
@@ -1,23 +1,182 @@
 from fuzzy_asteroids.fuzzy_asteroids import TrainerEnvironment
-
-from src.sample_controller import FuzzyController
-from sample_score import SampleScore
+import random
+from deap import base
+from deap import creator
+from deap import tools
+from typing import Tuple, Dict, Any
+import skfuzzy as fuzz
+import skfuzzy.control as ctrl
+import numpy
+from evaluator import evalscore
 
 if __name__ == "__main__":
     # Available settings
     settings = {
+        "graphics_on": True,
+        "sound_on": False,
         # "frequency": 60,
+        "real_time_multiplier": 200,
         # "lives": 3,
-        # "prints": False,
+        # "prints": True,
+        "allow_key_presses": False
     }
 
     # To use the controller within the context of a training solution
     # It is important to not create a new instance of the environment everytime
     game = TrainerEnvironment(settings=settings)
+    creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+    creator.create("Individual", list, fitness=creator.FitnessMax)
 
+    toolbox = base.Toolbox()
+
+    # Attribute generator
+    #                      define 'attr_bool' to be an attribute ('gene')
+    #                      which corresponds to integers sampled uniformly
+    #                      from the range [0,1] (i.e. 0 or 1 with equal
+    #                      probability)
+    toolbox.register("attr_output", random.randint, 1, 1000)
+
+    # Structure initializers
+    #                         define 'individual' to be an individual
+    #                         consisting of 100 'attr_bool' elements ('genes')
+    toolbox.register("individual", tools.initRepeat, creator.Individual,
+                     toolbox.attr_output, 5)
+
+    # define the population to be a list of individuals
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+
+#############THIS IS WHERE EVALSCORE USED TO BE#####################
+    #evalscore = evalscore("individual")
+
+    # ----------
+    # Operator registration
+    # ----------
+    # register the goal / fitness function
+    toolbox.register("evaluate", evalscore)
+
+    # register the crossover operator
+    toolbox.register("mate", tools.cxTwoPoint)
+
+    # register a mutation operator with a probability to
+    # flip each attribute/gene of 0.05
+    toolbox.register("mutate", tools.mutUniformInt, up=1000, low=1, indpb=0.2)
+
+    # operator for selecting individuals for breeding the next
+    # generation: each individual of the current generation
+    # is replaced by the 'fittest' (best) of three individuals
+    # drawn randomly from the current generation.
+    toolbox.register("select", tools.selRoulette)
+
+    # ----------
+    """
+    The Genetic Algorithm Itself
+    """
+
+    def main():
+        random.seed(42)
+
+        # create an initial population of 300 individuals (where
+        # each individual is a list of integers)
+        pop = toolbox.population(n=10)
+        hof_overall = tools.selBest(pop, 1)[0]
+
+        # CXPB  is the probability with which two individuals
+        #       are crossed
+        #
+        # MUTPB is the probability for mutating an individual
+        CXPB, MUTPB = 0.5, 0.2
+
+        print("Start of evolution")
+
+        # Evaluate the entire population
+        fitnesses = list(map(toolbox.evaluate, pop))
+        print(pop)
+        print(fitnesses)
+        print(len(pop))
+        print(len(fitnesses))
+        print(zip(pop, fitnesses))
+        for ind, fit in zip(pop, fitnesses):
+            ind.fitness.values = fit
+
+        print("  Evaluated %i individuals" % len(pop))
+
+        # Extracting all the fitnesses of
+        fits = [ind.fitness.values[0] for ind in pop]
+
+        # Variable keeping track of the number of generations
+        g = 0
+
+        # Begin the evolution
+        while g < 10:
+            # A new generation
+            g = g + 1
+            print("-- Generation %i --" % g)
+
+            # Select the next generation individuals
+            offspring = toolbox.select(pop, len(pop))
+            # Clone the selected individuals
+            offspring = list(map(toolbox.clone, offspring))
+
+            # Apply crossover and mutation on the offspring
+            for child1, child2 in zip(offspring[::2], offspring[1::2]):
+
+                # cross two individuals with probability CXPB
+                if random.random() < CXPB:
+                    toolbox.mate(child1, child2)
+
+                    # fitness values of the children
+                    # must be recalculated later
+                    del child1.fitness.values
+                    del child2.fitness.values
+
+            for mutant in offspring:
+
+                # mutate an individual with probability MUTPB
+                if random.random() < MUTPB:
+                    toolbox.mutate(mutant)
+                    del mutant.fitness.values
+
+            # Evaluate the individuals with an invalid fitness
+            invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+            fitnesses = map(toolbox.evaluate, invalid_ind)
+            for ind, fit in zip(invalid_ind, fitnesses):
+                ind.fitness.values = fit
+
+            print("  Evaluated %i individuals" % len(invalid_ind))
+
+            # The population is entirely replaced by the offspring
+            pop[:] = offspring
+
+            # Gather all the fitnesses in one list and print the stats
+            fits = [ind.fitness.values[0] for ind in pop]
+
+            length = len(pop)
+            mean = sum(fits) / length
+            sum2 = sum(x * x for x in fits)
+            std = abs(sum2 / length - mean ** 2) ** 0.5
+
+            print("  Min %s" % min(fits))
+            print("  Max %s" % max(fits))
+            print("  Avg %s" % mean)
+            print("  Std %s" % std)
+            hof_app = tools.selBest(pop, 1)[0]
+            if hof_app.fitness.values > hof_overall.fitness.values:
+                hof_overall = hof_app
+
+        print("-- End of (successful) evolution --")
+
+        best_ind = tools.selBest(pop, 1)[0]
+        print("Best individual of the last generation is %s, %s" % (best_ind, best_ind.fitness.values))
+        print("Best individual overall is %s, %s" % (hof_overall, hof_overall.fitness.values))
+
+if __name__ == "__main__":
+    main()
+    """
     for i in range(1000):
         # Call run() on an instance of the TrainerEnvironment
         # This function automatically manages cleanup
-        score = game.run(controller=FuzzyController(), score=SampleScore())
+        score = game.run(controller=FuzzyController(ControllerBase,individual), score=SampleScore())
 
         print(f"Generation {i}: {str(score)}")
+    """
